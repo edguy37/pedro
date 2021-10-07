4ded89f23748894a0f1bbac2fdf6eaee7d73d761
Enrique Gamboa <edguy37@gmail.com>
2021-10-07 18:52:59 -0500

    listar view created

--- app/Views/productos/listar.php
+++ app/Views/productos/listar.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
 </head>
 <body>
-    listar
+    Lista de productos
+    <div class="container">
+        <table class="table table-light">
+            <thead class="thead-light">
+                <tr>
+                    <th>#</th>
+                    <th>Descripcion</th>
+                    <th>Traduccion</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>ID</td>
+                    <td>Descripcion</td>
+                    <td>Traduccion</td>
+                    <td>Editar/Borrar</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
