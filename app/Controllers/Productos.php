d6007e399d8c930bf38541b59388a90bbc58db0e
Enrique Gamboa <edguy37@gmail.com>
2021-10-08 00:47:59 -0500

    now working with template. footer and header created

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -10,6 +10,9 @@ class Productos extends Controller{
         $producto = new Producto();
         $datos['productos'] = $producto->orderBy('id', 'ASC')->findAll();
 
+        $datos['header'] = view('template/header'); //$datos['cualquierNombre'] no es obligatorio que se llamen igual
+        $datos['footer'] = view('template/footer');
+
         return view('productos/listar', $datos);
     }
 
--- app/Views/productos/listar.php
+++ app/Views/productos/listar.php
@@ -1,23 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document Title</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-</head>
-<body>
-    Lista de productos
-    <div class="container">
-        <table class="table table-light">
-            <thead class="thead-light">
-                <tr>
-                    <th>#</th>
-                    <th>Descripcion</th>
-                    <th>Traduccion</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
+<?php echo $header; ?>
+    <table class="table table-light">
+        <thead class="thead-light">
+            <tr>
+                <th>#</th>
+                <th>Descripcion</th>
+                <th>Traduccion</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
             <tbody>
                 <?php foreach($productos as $producto): ?>
                 <tr>
@@ -28,7 +18,5 @@
                 </tr>
                 <?php endforeach; ?>
             </tbody>
-        </table>
-    </div>
-</body>
-</html>
+    </table>
+<?php echo $footer; ?>
